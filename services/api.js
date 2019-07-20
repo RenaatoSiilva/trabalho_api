684ea8e854451d788861de6977eecbe3cc96b02b
Renato Silva <renato_silva94@live.com>
2019-07-20 11:09:24 -0300

    Alteração no Token

--- services/api.js
+++ services/api.js
@@ -1,21 +1,10 @@
 import axios from 'axios'
 const __API__ = 'https://api.github.com'
 
-// const instance = axios.create({
-//   baseURL: __API__
-// })
-
-const instance = () => {
-  return axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {
-      'Authorization' : 'token 6c71786999255509d10a1c29c2c8858cbddec005 '
-    }
-  })
-}
-
-export default instance()
-
-
-
-
+const instance = axios.create({
+  baseURL: __API__,
+  headers: {
+    'Authorization' : 'token 43e1ee475d8e51fc80ffa3c908800bc48b4d0f80'
+  }
+})
+export default instance
