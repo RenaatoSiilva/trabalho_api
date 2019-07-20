1f885152781e43b4c7dcb1b7cbc173ed78cc2abb
mayander30 <mayander30@gmail.com>
2019-07-20 14:10:32 -0300

    search

--- services/git.js
+++ services/git.js
@@ -3,3 +3,5 @@ import api from './api'
 export const getReposUser = user => api.get(`/users/${user}/repos`) 
 
 export const getRepoFromUser = (user_repo) => api.get(`/repos/${user_repo}`)
+
+export const getIssuesFromRepo = (user_repo) => api.get(`/${user_repo}/issues`);
